f739a58ced298898f385eda0e6dbcbae0a74f273
Naveen2301 <naveenkumarkota97@gmail.com>
2024-04-18 18:22:28 +0530

    first commit

---
+++ src/main/java/org/scaler/backend/paginationsearchfilter/PaginationSearchFilterApplication.java
@@ -0,0 +1,13 @@
+package org.scaler.backend.paginationsearchfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSearchFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSearchFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/scaler/backend/paginationsearchfilter/PaginationSearchFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scaler.backend.paginationsearchfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSearchFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
